a50cba7ff06689d8f03ff3bdbb1d5b7fe17c9dd3
AlexandraEL9 <alexandraelee9@gmail.com>
2025-08-15 20:13:12 +0100

    add scss and variables

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.scss';
 import App from './App';
+import './index.scss';
+
+
+// Import global SCSS here
+//import './sass/.scss'; // import SCSS entry point
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
