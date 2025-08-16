a835895ce15684a73a59f842820dc72f09cb8a60
AlexandraEL9 <alexandraelee9@gmail.com>
2025-08-16 16:21:25 +0100

    add tasks fieldset and check is red on save

--- frontend/src/components/addRoutineForm/AddRoutineForm.js
+++ frontend/src/components/addRoutineForm/AddRoutineForm.js
@@ -3,14 +3,14 @@ import React, { useId, useState } from "react";
 function AddRoutineForm() {
   const [name, setName] = useState(""); 
   const nameId = useId();
-
+  // create fields for task objects
+  const [tasks, setTasks] = useState([{ id: 1, title: "", duration: "00:30" }]);
   // Handle page submission
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ name: name.trim() });
-    alert("Saved! (check console)");
-  };
-
+    console.log({ name, tasks });
+    alert("Saved (check console)");
+ };
   return (
     <form onSubmit={handleSubmit}>
       <div>
@@ -24,6 +24,17 @@ function AddRoutineForm() {
           required
         />
       </div>
+    {/* field set, and inputs for tasks within the routine */} 
+      <fieldset>
+        <legend>Tasks</legend>
+        <div>
+          <span>1.</span>{" "}
+          <input type="text" placeholder="Task name" value={tasks[0].title} />{" "}
+          <input type="text" placeholder="mm:ss" value={tasks[0].duration} />{" "}
+          <button type="button" disabled>Remove</button>
+        </div>
+        <button type="button" disabled>Add task</button>
+      </fieldset>
 
       <div style={{ marginTop: 16 }}>
         <button type="submit">Save</button>
